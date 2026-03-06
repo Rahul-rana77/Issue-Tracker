68a13cc922d9569ee6ce0cd1d56edb4977b75377
Rahul Rana <rahulranawork11@gmail.com>
2026-03-07 00:24:14 +0530

    fix error

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/issue', issueRoutes);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
