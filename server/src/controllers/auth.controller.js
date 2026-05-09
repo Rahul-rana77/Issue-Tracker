aed08355c6aff61b9aa4b2c719e27420082754e7
Rahul Rana <rahulranawork11@gmail.com>
2026-05-09 16:52:42 +0530

    fix

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -44,7 +44,7 @@ const registerUser = async (req, res) => {
         }
 
         const token = jwt.sign({
-                        id: user._id,
+                        id: existingUser._id,
                     }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Add token expiration for security
 
                     res.cookie("token", token, {
