985105aa027791727a1be98943a497b1bfa923ee
Rahul Rana <rahulranawork11@gmail.com>
2026-05-13 01:13:52 +0530

    fix: remove duplicate isAdmin field from user schema

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
     emailVerificationCode: {
         type: String,
     },
